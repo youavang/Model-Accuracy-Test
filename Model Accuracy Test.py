9abc12ede6271dffa7b0656d2d0194c1ebeee4eb
Youa <youa.v@outlook.com>
2020-03-27 16:09:00 -0500

    Update Model Accuracy Test.py

--- Model Accuracy Test.py
+++ Model Accuracy Test.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # In this program, we are going to test several models to see which is best for a categorical dataset.
-
-# In[3]:
-
 
 # Import libraries.
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# In[4]:
-
 
 #Load the dataset and split into training (75%) and test (25%) sets.
 from sklearn.datasets import load_digits
@@ -32,9 +22,6 @@ X_test = sc_X.transform(X_test)
 
 # 1.) Train Logistic Regression model on the dataset and print the accuracy of the model using the score method.
 
-# In[5]:
-
-
 #Fitting Logistic Regression to Training Set
 from sklearn.linear_model import LogisticRegression
 classifierObj = LogisticRegression(random_state=0, multi_class = 'auto')
@@ -51,14 +38,10 @@ print('Logistic Regression Accuracy Score =', a)
 
 # 2.) Train SVM with linear kernel and print the accuracy of the model.
 
-# In[6]:
-
-
 #Fitting Classifier to Training Set. Create a classifier object here and call it classifierObj
 from sklearn.svm import SVC
 classifierObj = SVC(kernel = 'linear')
 classifierObj.fit(X_train, y_train)
-
 
 #Making predictions on the Test Set
 y_pred = classifierObj.predict(X_test)
@@ -68,12 +51,8 @@ from sklearn.metrics import accuracy_score
 a = accuracy_score(y_test, y_pred)
 print('SVM Linear kernel Accuracy Score =',a)
 
-
 # 3.) For the SVM with poly kernel, we will write a loop trying different values of degree and train to find the kernel with the best accuracy. Also, we'll plot a graph and analyze the trend.
 #     * What value of degree gives you the best accuracy?
-
-# In[7]:
-
 
 for degree in range(10): 
     scores = []
@@ -107,14 +86,10 @@ plt.show()
 
 # 4.) Train SVM with RBF kernel and print the accuracy of the model.
 
-# In[8]:
-
-
 #Fitting Classifier to Training Set. Create a classifier object here and call it classifierObj
 from sklearn.svm import SVC
 classifierObj = SVC(kernel = 'rbf', gamma = 'auto')
 classifierObj.fit(X_train, y_train)
-
 
 #Making predictions on the Test Set
 y_pred = classifierObj.predict(X_test)
@@ -124,12 +99,8 @@ from sklearn.metrics import accuracy_score
 a = accuracy_score(y_test, y_pred)
 print('SVM with RBF kernel Accuracy Score =',a)
 
-
 # 5.) For the classification using k-NN, we will write a loop trying different values of k and we'll plot a graph to analyze the trend.
 #     * What value of k gives you the best accuracy?
-
-# In[9]:
-
 
 for k in range(1,31):
     scores = []
@@ -157,19 +128,14 @@ for k in range(1,31):
 
 plt.show()
 
-
 # k = 1 and 5 gives the best accuracy of 97.3%. According to the graph, as k increases, accuracy decreases.
 
 # 6.) Train Naive Bays Model and print the accuracy of the model.
-
-# In[10]:
-
 
 #Fitting Classifier to Training Set. Create a classifier object here and call it classifierObj
 from sklearn.naive_bayes import GaussianNB
 classifierObj = GaussianNB()
 classifierObj.fit(X_train, y_train)
-
 
 #Making predictions on the Test Set
 y_pred = classifierObj.predict(X_test)
@@ -179,11 +145,7 @@ from sklearn.metrics import accuracy_score
 a = accuracy_score(y_test, y_pred)
 print('Naive Bayes Accuracy Score =', a)
 
-
 # 7.) Train Decision Tree and print the accuracy of the model.
-
-# In[11]:
-
 
 #Fitting Classifier to Training Set. Create a classifier object here and call it classifierObj
 from sklearn.tree import DecisionTreeClassifier
@@ -198,12 +160,8 @@ from sklearn.metrics import accuracy_score
 a = accuracy_score(y_test, y_pred)
 print('Decision Tree Accuracy Score =', a)
 
-
 # 8.) For the Random Forest, we will write a loop trying different values of n_esitmators plot a graph to analyze the trend.
 #     * What values of n_estimators gives you the best accuracy?
-
-# In[12]:
-
 
 for estimator in range(1,101):
     scores = []
@@ -231,7 +189,6 @@ for estimator in range(1,101):
     
 plt.show()
 
-
 # You can have an inifinite number of estimators, but for the purpose of this demonstration I chose a range of 100 estimators. For this range, the best estimators are 72, 73, 77 and 78 all with a 98.2% accuracy. According to the graph, the more estimators you have, the more likely you will get more accuracy, however accuracy plateau after certain amount of estimators.
 
 # Here are the best accuracy for each model:
@@ -245,9 +202,3 @@ plt.show()
 # * Random Forest Classification = 0.9822222222222222
 
 # When comparing the accuracy scores of all the models, SVM with rbf kernel produced the highest accuracy score for this dataset.
-
-# In[ ]:
-
-
-
-
